f2d544d9b670bc56db47b2029af5d731585724fb
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-17 13:40:39 +0700

    Did not keep track of the commit, nth commit\

---
+++ ConditionalStatements/5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int edge;
+    printf("Please enter the edge of the square: ");
+    scanf("%d", &edge);
+    edge <= 0 && printf("The edge must be a positive number!") && exit;
+    printf("The perimeter of the square is %d", edge * 4);
+    printf("\nThe area      of the square is %d", edge * edge);
+}
