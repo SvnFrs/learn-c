c58a59b362394a800497fdb14bc5742ba3e43269
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-17 13:26:24 +0700

    Did not keep track of the commit, nth commit\

---
+++ ConditionalStatements/4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int first, second;
+    printf("Please enter first number : ");
+    scanf("%d", &first);
+    printf("\n");
+    printf("Please enter second number: ");
+    scanf("%d", &second);
+    second > first && printf("Comparison results: %d is greater than %d", second, first) && exit;
+    second < first && printf("Comparison results: %d is less than %d", second, first) && exit;
+    second == first && printf("Comparison results: %d is equal to %d", second, first) && exit;
+}
