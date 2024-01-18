61555ced28f44b95c6d4e18cf422e7bb3a395e3f
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-18 23:13:49 +0700

    Did not keep track of the commit, nth commit\

---
+++ LoopStatements/31/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int input;
+    scanf("%d", &input);
+    
+    input <= 0 && printf("N must be greater than 0!") && exit;
+
+    for (int i = 1; i <= input; i++)
+    {
+        printf("%d ", i);
+        for (int j = i-1; j > 0; j--)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+        
+    }
+}
