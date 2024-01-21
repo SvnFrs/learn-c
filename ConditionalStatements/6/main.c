fbd30c8be929c5e6b9a2acb7a4054da65da31252
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-01-21 23:43:35 +0700

    Did not keep track of the commit, nth commit\

--- ConditionalStatements/6/main.c
+++ ConditionalStatements/6/main.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int a, b, c, perimeter;
-    double area;
+    int width, height;
+    printf("Enter width: ");
+    scanf("%d", &width);
+    printf("Enter height: ");
+    scanf("%d", &height);
 
-    printf("Enter the first side of triangle: ");
-    scanf("%d", &a);
-    printf("Enter the second side of triangle: ");
-    scanf("%d", &b);
-    printf("Enter the third side of triangle: ");
-    scanf("%d", &c);
-    if (a <= 0 || b <= 0 || c <= 0)
-    {
-        printf("These three numbers must be a positive number!\n");
-        return 1;
-    }
-    if (a + b > c && a + c > b && b + c > a) {
-        perimeter = a + b + c;
-        printf("Perimeter of triangle is %d\n", perimeter);
-
-        double s = perimeter / 2.0;
-        area = sqrt(s * (s - a) * (s - b) * (s - c));
-        printf("Area of triangle is %f\n", area);
-    } else {
-        printf("Please enter valid sides of triangle.\n");
-    }
-
-    return 0;
+    int area = width * height;
+    printf("Area: %d\n", area);
+    int perimeter = 2 * (width + height);
+    printf("Perimeter: %d\n", perimeter);
 }
